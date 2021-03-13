01f507e15faea391d71cea1acf23603c14a21181
avurlasaikumar <avurlasaikumar@gmail.com>
2021-03-13 20:08:49 +0530

    Password hashing implementation

--- LoudInSight/Controllers/LoginController.cs
+++ LoudInSight/Controllers/LoginController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using LoudInSight.BusinessObject.Interfaces;
 using LoudInSight.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LoudInSight.api.Controllers
 {
@@ -22,10 +26,42 @@ namespace LoudInSight.api.Controllers
         }
         [HttpPost]
         [Route("Login")]
-        public string Login(Login login)
+        public IActionResult Login(Login login)
         {
-           var _login =  loginManager.Login(login);
-            return "Register";
+            var passwordHasher = new PasswordHasher<Login>();
+            string password = login.Password;
+            var hasedPassword = passwordHasher.HashPassword(login, login.Password);
+            login.Password = hasedPassword;
+            var user =  loginManager.Login(login);
+            if (user != null && passwordHasher.VerifyHashedPassword(user, hasedPassword, password) == Microsoft.AspNetCore.Identity.PasswordVerificationResult.Success)
+            {
+                
+                //if (!string.IsNullOrEmpty(ReturnUrl) && Url.IsLocalUrl(ReturnUrl))
+                //{
+                //    return LocalRedirect(ReturnUrl);
+                //}
+                //return RedirectToAction("index", "home");
+
+                var authClaims = new List<Claim> {
+                        new Claim(ClaimTypes.Name,user.Email),
+                        new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                    };
+                var authSigninKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretApptest12348576"));
+                var token = new JwtSecurityToken(
+                    issuer: "http://localhost:63789",
+                    audience: "User",
+                    expires: DateTime.Now.AddMinutes(30),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigninKey, SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            else
+            {
+                return Unauthorized(user);
+            }
+            
         }
     }
+
 }
